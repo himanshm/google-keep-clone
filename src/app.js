7af2d37f80c7912ff3312ffcc6f03e12d283d52f
Himanshu Mishra <sai.himanshum011@gmail.com>
2025-07-17 20:07:19 +0530

    Store the notes in the app

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 class App {
   constructor() {
+    this.notes = []; // Array to hold notes
     this.$form = document.querySelector('#form'); // Element for the form
     this.$noteTitle = document.querySelector('#note-title'); // Element for the note title
+    this.$noteText = document.querySelector('#note-text'); // Element for the note text
     this.$formButtons = document.querySelector('#form-buttons'); // Elements for the form buttons
     this.addEventListeners();
   }
@@ -9,6 +11,17 @@ class App {
   addEventListeners() {
     document.body.addEventListener('click', (event) => {
       this.handleFormClick(event);
+    });
+
+    this.$form.addEventListener('submit', (event) => {
+      event.preventDefault(); // Prevent the default form submission
+      const title = this.$noteTitle.value.trim();
+      const text = this.$noteText.value.trim();
+
+      const hasNote = title || text; // Check if there's any note content
+      if (hasNote) {
+        this.addNote({ title, text });
+      }
     });
   }
 
@@ -34,6 +47,17 @@ class App {
     this.$form.classList.remove('form-open');
     this.$noteTitle.style.display = 'none';
     this.$formButtons.style.display = 'none';
+  }
+
+  addNote(note) {
+    const newNote = {
+      title: note.title,
+      text: note.text,
+      color: '#fff', // Default color
+      id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1, // Date.now(), // Unique ID based on timestamp
+    };
+    this.notes = [...this.notes, newNote]; // Add the new note to the notes array
+    console.log(this.notes);
   }
 }
 
